2dd06d7eb8ba2bb41a2ef018e957d11aeb9fec71
Noah2211 <noah.decroix3@gmail.com>
2021-12-03 14:56:57 +0100

    full JS done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,4 @@
 const word = [
-    "teeeest",
-    /*
     "revoir",
     "hello",
     "world",
@@ -16,8 +14,8 @@ const word = [
     "titan",
     "colossal",
     "bestial",
-    "inspiration",
-*/
+    "inspiration"
+
 ];
 
 let looseCount = 2;
@@ -26,25 +24,24 @@ let id = 0;
 let wordRandom;
 let random = Math.floor(Math.random() * word.length);
 const pendu = document.getElementById("pendu");
-const container = document.getElementById("sous-container1");
+const containerForButton = document.getElementById("sous-container1");
 let letterUser;
 const inputUser = document.getElementById("inputLetter");
 const sendLetterButton = document.getElementById("sendLetter");
+const containerForLetter = document.getElementById("container-letter");
 
-
+let winText = document.getElementById("win");
 let createDiv = document.createElement("div");
 
 let restartButton = document.createElement("button");
 restartButton.innerHTML = "Re-start";
-container.append(restartButton);
+containerForButton.append(restartButton);
 
 restartButton.addEventListener("click", function () {
-    looseCount = 1;
-    pendu.src = "/assets/img/" + looseCount + ".png";
-    randomWord();
+    restartGame();
 });
 
-
+// Function
 function randomWord() {
     random = Math.floor(Math.random() * word.length);
     wordRandom = word[random];
@@ -54,13 +51,14 @@ function randomWord() {
 
 randomWord();
 
+//Function for create a Div for each letter of the random word
 function createDivForEachLetter() {
     for (let i = 0; i < wordRandom.length; i++) {
         createDiv = document.createElement("div");
         createDiv.classList.add("letter");
         createDiv.innerHTML = "?";
         createDiv.id = `${id++}`;
-        container.appendChild(createDiv);
+        containerForLetter.appendChild(createDiv);
     }
     document.getElementById("0").innerHTML = wordRandom[0];
     document.getElementById((wordRandom.length - 1).toString()).innerHTML = wordRandom[wordRandom.length - 1];
@@ -75,33 +73,39 @@ sendLetterButton.addEventListener("click", function () {
     checkLetter();
 })
 
+
+//Function for win or loose condition
 function checkLetter() {
     if (wordRandom.includes(letterUser)) {
         printLetter();
         if (winCount === wordRandom.length) {
-            document.getElementById("win").innerHTML = "T'as épargne Kenny bien joué enfoiré";
-            restartGame();
+            winText.innerHTML = "T'as épargne Kenny bien joué enfoiré";
         }
     }
-    else {
+    else if (!wordRandom.includes(letterUser)) {
+        winText.innerHTML += `${letterUser} n'est pas dans le mot` + "<br>";
         pendu.src = "/assets/img/" + looseCount++ + ".png";
-        if (looseCount === 9) {
-            console.log("ta tué kenny enfoiré");
-            restartGame();
+        if (looseCount === 8) {
+            winText.innerHTML = `T'a buté kenny enfoiré le bon mot c'était ${wordRandom.toUpperCase()}`;
+            sendLetterButton.style.display = "none";
+
         }
     }
 }
-
+//Function for restart all game, generate a random word again and make an empty divs etc...
 function restartGame() {
     looseCount = 1;
+    pendu.src = "/assets/img/" + looseCount + ".png";
     winCount = 2;
-    pendu.src = "/assets/img/" + looseCount++ + ".png";
-
+    id = 0;
+    containerForLetter.innerHTML = "";
+    winText.innerHTML = "";
+    sendLetterButton.style.display = "inline";
     randomWord();
-
+    createDivForEachLetter();
 }
 
-
+//Function for print each letter on the right div.
 function printLetter() {
     for (let i = 1; i < wordRandom.length - 1; i++) {
         if (letterUser === wordRandom[i]) {
